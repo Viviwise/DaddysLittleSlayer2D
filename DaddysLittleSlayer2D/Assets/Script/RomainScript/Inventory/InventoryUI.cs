933a60c42b6643a0bb8c0dfc4741027a8c17f8de
Yharnam37 <romain.rousseau21110@gmail.com>
2025-11-13 12:24:56 +0100

    Inventory 3.5/4 fait

---
+++ DaddysLittleSlayer2D/Assets/Script/RomainScript/Inventory/InventoryUI.cs
@@ -0,0 +1,65 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryUI : MonoBehaviour 
+{ 
+    [SerializeField] private Inventory inventory; 
+
+    [SerializeField] private GameObject uiPrefab;
+    [SerializeField] private InventoryItemData Data;
+
+    public int quantity;
+    public Image itemDescriptionImage;
+    public TMP_Text itemDescriptionNameText;
+    public TMP_Text itemDescriptionText;
+    [SerializeField] private TMP_Text quantityText;
+
+    public void Sync() 
+    { 
+        foreach (Transform child in transform) 
+            Destroy(child.gameObject);
+        
+        foreach ((InventoryItemData itemPair, int quantity) in inventory.items) 
+        { 
+            GameObject uiItem = Instantiate(uiPrefab, transform); 
+            inventoryItemUI uiItemComponent = uiItem.GetComponent<inventoryItemUI>();
+            uiItemComponent.SetReferences(itemDescriptionImage, itemDescriptionText, itemDescriptionNameText);
+            uiItemComponent.SetItem(itemPair, quantity); 
+        }
+
+        EmptySlot();
+    }
+    public void EmptySlot()
+    {
+        itemDescriptionImage.sprite = null ;
+        itemDescriptionNameText.text = "";
+        itemDescriptionText.text = "";
+        itemDescriptionImage.sprite = null;
+        Debug.Log("Empty Slot");
+    }
+
+    public void DeselectAllSlots() 
+    { 
+        foreach (Transform child in transform) 
+        { 
+            inventoryItemUI uiItemComponent = child.GetComponent<inventoryItemUI>(); 
+            uiItemComponent.selectShader.SetActive(false); 
+            uiItemComponent.thisItemSelected = false;
+        }
+        EmptySlot();
+    }
+
+    public void UseWeapon(InventoryItemData inventoryItemData)
+    {
+        Inventory.Instance.WeaponSlot.Data = inventoryItemData;
+        InventoryItemData weaponData = Inventory.Instance.WeaponSlot.Data;
+    }
+
+    public void UseConsumable(InventoryItemData inventoryItemData)
+    {
+        Inventory.Instance.ConsumableSlot.Data = inventoryItemData;
+        InventoryItemData consumableSlot = Inventory.Instance.ConsumableSlot.Data;
+    }
+
+
+}
