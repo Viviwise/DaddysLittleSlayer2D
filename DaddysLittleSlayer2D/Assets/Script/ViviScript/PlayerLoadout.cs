10d78a632bcb96b8be44f5c3759c1931328bd328
Vivien <viviane-rohaut@outlook.fr>
2025-11-14 13:05:01 +0100

    UI Fini

--- DaddysLittleSlayer2D/Assets/Script/ViviScript/PlayerLoadout.cs
+++ DaddysLittleSlayer2D/Assets/Script/ViviScript/PlayerLoadout.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerLoadout : MonoBehaviour
 {
-    public InventoryItemData[] selectedItems = new InventoryItemData[1];
+    public InventoryItemData[] selectedItems = new InventoryItemData[2];
     public void Sync()
     {
         Inventory.Instance.WeaponSlot.Data = selectedItems[0];
